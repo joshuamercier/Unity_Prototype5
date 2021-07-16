70bf6b08c567d44e8a25fa5f224481e1cdeb89e2
Josh <joshuamercier@yahoo.com>
2021-07-16 18:58:27 -0400

    Added objects for gameplay, good and bad. Objects delete when out of bounds or when clicked on. Objects spawn randomly, random positions, random torgue, random force added. UI score added, not fully implemented yet.

--- Prototype5/Assets/Scripts/Target.cs
+++ Prototype5/Assets/Scripts/Target.cs
@@ -4,15 +4,52 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
+    // Class variables
+    private Rigidbody targetRb;
+    private float minSpeed = 12;
+    private float maxSpeed = 16;
+    private float maxTorgue = 10;
+    private float xRange = -4;
+    private float ySpawnPos = -2;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        targetRb = GetComponent<Rigidbody>();
+
+        targetRb.AddForce(RandomForce(), ForceMode.Impulse);
+        targetRb.AddTorque(RandomTorgue(), RandomTorgue(), RandomTorgue(), ForceMode.Impulse);
+        transform.position = RandomSpawnPos();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);
+    }
+
+    float RandomTorgue()
+    {
+        return Random.Range(-maxTorgue, maxTorgue);
+    }
+
+    Vector3 RandomSpawnPos()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), ySpawnPos, 0);
+    }
+
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
     }
 }
