5ae5d56eaa5559a4b0e99134e283881a45c55b07
Josh <joshuamercier@yahoo.com>
2021-07-16 21:32:51 -0400

    Score UI works. Added particle and scores to the different objects.

--- Prototype5/Assets/Scripts/Target.cs
+++ Prototype5/Assets/Scripts/Target.cs
@@ -5,17 +5,22 @@ using UnityEngine;
 public class Target : MonoBehaviour
 {
     // Class variables
+    public int pointValue;
+    public ParticleSystem explosionParticle;
+
     private Rigidbody targetRb;
     private float minSpeed = 12;
     private float maxSpeed = 16;
     private float maxTorgue = 10;
     private float xRange = -4;
     private float ySpawnPos = -2;
+    private GameManager gameManager;
 
     // Start is called before the first frame update
     void Start()
     {
         targetRb = GetComponent<Rigidbody>();
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
 
         targetRb.AddForce(RandomForce(), ForceMode.Impulse);
         targetRb.AddTorque(RandomTorgue(), RandomTorgue(), RandomTorgue(), ForceMode.Impulse);
@@ -46,6 +51,8 @@ public class Target : MonoBehaviour
     private void OnMouseDown()
     {
         Destroy(gameObject);
+        gameManager.UpdateScore(pointValue);
+        Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
     }
 
     private void OnTriggerEnter(Collider other)
